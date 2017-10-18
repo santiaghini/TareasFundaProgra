65aa04475e85ff7ec533ffbdea57f425b88fe4bf
Santiago Hernández <santi.hergut@gmail.com>
2017-10-18 11:42:15 -0500

    Update e4t2.java
    New

--- arreglos_2/e4t2.java
+++ arreglos_2/e4t2.java
@@ -3,27 +3,27 @@ public class e4t2{
 
     int b,k;
     int arrl [] = new int [80];
-    k=0;
+    k = 0;
     int i = 0;
 
 
     for (int j = 1 ;k<80 ;j++ ) {
-        b=0;
+        b = 0;
 
         for (int d = 1 ; d<=1000000 ; d++ ) {
           if (j%d == 0) {
-              b=b+1;
+              b = b+1;
           }
         }
-          if (b==2) {
+          if (b == 2) {
             //System.out.printf("El número %d es primo.%n ",j);
-            k = k+1;
+            k = k + 1;
             arrl[i]=j;
             i++;
       }
     }
     for (int m = 0;m<arrl.length ;m++ ) {
-      System.out.printf("%d,\t", arrl[m]);
+      System.out.printf("%d \n", arrl[m]);
     }
   }
 }
